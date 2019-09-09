ebdd638ce6bd182eb1a70ab9fc14c5ab077db012
Magesh Kuppan <tkmagesh77@gmail.com>
2019-09-09 15:32:34 +0530

    Update closures.js

--- 01-JavaScript/closures.js
+++ 01-JavaScript/closures.js
@@ -40,5 +40,50 @@ var isPrime = (function(){
 	}
 })();
 
+var isOddOrEven = (function(){
+	var cache={};
+
+	function checkOddOrEven(n){
+		console.log('processing ', n)
+		return n % 2 === 0 ? 'even' : 'odd';
+	}
+
+	return function(n){
+		if (!cache.hasOwnProperty(n))
+			cache[n] = checkOddOrEven(n);
+		return cache[n];
+	}
+})();
 
 
+function memoize(fn){
+	var cache={};
+
+	return function(n){
+		if (!cache.hasOwnProperty(n))
+			cache[n] = fn(n);
+		return cache[n];
+	}
+}
+
+
+var isOddOrEven = memoize(function checkOddOrEven(n){
+	console.log('processing ', n)
+	return n % 2 === 0 ? 'even' : 'odd';
+})
+
+function memoize(fn){
+	var cache={};
+
+	return function(){
+		var key = JSON.stringify(arguments);
+		if (!cache.hasOwnProperty(key))
+			cache[key] = fn.apply(this, arguments);
+		return cache[key];
+	}
+}
+
+var add = memoize(function(x,y){
+	console.log('processing ', x ,' and ', y);
+	return x + y;
+})
